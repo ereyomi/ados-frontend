d51e3d79d7532a4dc174bda9e6e2f0b80ab2001b
Ereyomi Oluwaseyi <ereyomioluwaseyi@gmail.com>
2020-11-22 21:49:20 +0100

    feat: working on route / create card component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StyleComponent } from './style/style.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'style'
+  },
+  {
+    path: 'style',
+    component: StyleComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './shared/card/card.component';
+import { StyleComponent } from './style/style.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    StyleComponent
   ],
   imports: [
     BrowserModule,
