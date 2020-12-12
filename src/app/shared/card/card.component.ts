c203ce2286a407ded30874ce1af426174d8be6ee
Ereyomi Oluwaseyi <ereyomioluwaseyi@gmail.com>
2020-12-12 09:56:50 +0100

    feat: shop cards completed with styles

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -7,6 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  products = [
+    {
+      image: 'assets/images/product4.jpg',
+      name: 'iphone 6',
+      rating: 5,
+      price: '866',
+      currency: 'USD',
+      currencySymbol: '$'
+    },
+    {
+      image: 'assets/images/product4.jpg',
+      name: 'iphone 6',
+      rating: 5,
+      price: '866',
+      currency: 'USD',
+      currencySymbol: '$'
+    },
+    {
+      image: 'assets/images/product4.jpg',
+      name: 'iphone 6',
+      rating: 5,
+      price: '866',
+      currency: 'USD',
+      currencySymbol: '$'
+    },
+    {
+      image: 'assets/images/product4.jpg',
+      name: 'iphone 6',
+      rating: 5,
+      price: '866',
+      currency: 'USD',
+      currencySymbol: '$'
+    },
+    {
+      image: 'assets/images/product4.jpg',
+      name: 'iphone 6',
+      rating: 5,
+      price: '866',
+      currency: 'USD',
+      currencySymbol: '$'
+    }
+  ]
+  product = {
+    image: 'assets/images/product4.jpg',
+    name: 'iphone 6',
+    rating: 5,
+    price: '866',
+    currency: 'USD',
+    currencySymbol: '$'
+  }
   constructor() { }
 
   ngOnInit(): void {
