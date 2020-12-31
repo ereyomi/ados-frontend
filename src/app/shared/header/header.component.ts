243c64066702d7659a0a505621a378e379c3cd86
Ereyomi Oluwaseyi <ereyomioluwaseyi@gmail.com>
2020-12-31 22:11:36 +0100

    refactor: little things

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from 'src/app/core/services/products.service';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private pS: ProductsService) { }
+  openBag(): void {
+    this.pS.openBagModal();
+  }
   ngOnInit(): void {
   }
 
